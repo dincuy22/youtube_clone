c7ca6f06aa8759a13d8a598c06e71567adb82588
dincuy22 <dincuy2022@gmail.com>
2023-05-12 14:05:31 +0000

    create component Videos

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Stack, Box, Typography } from "@mui/material";
 
-import Sidebar from "./Sidebar";
+import { Sidebar, Videos } from "./";
 
 function Feed() {
   return (
@@ -21,6 +21,19 @@ function Feed() {
         >
           Copyright 2023 Dincuy Media
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New <span style={{ color: "#F31503" }}>Videos</span>
+        </Typography>
+
+        <Videos />
       </Box>
     </Stack>
   );
